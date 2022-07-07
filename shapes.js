199026b12d9192b1a297347873643fcb29d0fb61
ashutosh-mrk <108858423+ashutosh-mrk@users.noreply.github.com>
2022-07-07 19:40:56 +0530

    Add files via upload

---
+++ shapes.js
@@ -0,0 +1,33 @@
+function area(){
+var v1= document.getElementById('v1').value;
+var result=(3.14*+v1*+v1);
+document.getElementById('v2').value = result;
+}
+
+function perimeter(){
+var v1= document.getElementById('v1').value;
+var result=(2*3.14*+v1);
+document.getElementById('v3').value = result;
+}
+
+
+
+
+
+// area of rectangle
+function ar(){
+		var v1=document.getElementById("c1").value;
+	var v2=document.getElementById("c2").value;
+	var result = (+v1 * +v2);
+	console.log(v1);
+	document.getElementById("v5").value = result;
+	
+}
+
+function pr(){
+	var v1= document.getElementById("c1").value;
+	var v2=document.getElementById("c2").value;
+	var result = 2*(+v1 + +v2);
+	document.getElementById("v6").value = result;
+	
+}
